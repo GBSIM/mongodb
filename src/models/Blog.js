e1ea7107d1824eca2b2e91736d2b2827be396633
Gyubin <gbcompany27@gmail.com>
2022-08-11 18:54:03 +0900

    add text index

--- src/models/Blog.js
+++ src/models/Blog.js
@@ -5,15 +5,19 @@ const BlogSchema = new Schema({
     title: {type: String, required: true},
     content: {type: String, required: true},
     islive: {type: Boolean, required: true, default: false},
-    user: {_id: {type: Types.ObjectId, required: true, ref: 'user'},
-          username: {type: String, required: true},
-          name: {
-            first: {type:String, required: true},
-            last: {type: String, required: true}
-          },
-        },
+    user: {
+      _id: {type: Types.ObjectId, required: true, ref: 'user'},
+      username: {type: String, required: true},
+      name: {
+        first: {type:String, required: true},
+        last: {type: String, required: true}
+      },
+    },
     comments: [CommentSchema],
 },{timestamps: true})
+
+BlogSchema.index({ 'user._id': 1, updateAt: 1 })
+BlogSchema.index({ title: "text" })
 
 BlogSchema.set("toObject", {virtuals: true});
 BlogSchema.set("toJSON", {virtuals: true});
