5c99c02d7dece8fda92191eb6666b68143acca41
Gyubin <gbcompany27@gmail.com>
2022-08-18 22:41:50 +0900

    add session

--- src/routes/commentRoute.js
+++ src/routes/commentRoute.js
@@ -1,50 +1,56 @@
 const {Router} = require('express');
 const commentRouter = Router({mergeParams: true});
 const {User, Blog, Comment} = require('../models');
-const {isValidObjectId} = require('mongoose');
+const {isValidObjectId, startSession} = require('mongoose');
 
 commentRouter.post('/', async (req,res) => {
+    const session = await startSession();
+    let comment;
     try {
-        const {blogId} = req.params;
-        const {content, userId} = req.body;
-        if (typeof content !== 'string') return res.status(400).send({err: "content is required"})
-        if (!isValidObjectId(userId)) return res.status(400).send({err: "userId is invalid"})
-        if (!isValidObjectId(blogId)) return res.status(400).send({err: "blogId is invalid"})
-
-        const [user, blog] = await Promise.all([
-            User.findById(userId),
-            Blog.findById(blogId)
-        ])
-
-        if (!user) return res.status(400).send({err: "user doest not exist"})
-        if (!blog) return res.status(400).send({err: "blog doest not exist"})
-
-        if (!blog.islive) return res.status(400).send({err: "blog is not available"})
-        
-        let comment = new Comment({
-            content,
-            user, 
-            userFullName:`${user.name.first} ${user.name.last}`, 
-            blog: blogId
+        await session.withTransaction(async() => {
+            const {blogId} = req.params;
+            const {content, userId} = req.body;
+            if (typeof content !== 'string') return res.status(400).send({err: "content is required"})
+            if (!isValidObjectId(userId)) return res.status(400).send({err: "userId is invalid"})
+            if (!isValidObjectId(blogId)) return res.status(400).send({err: "blogId is invalid"})
+    
+            const [user, blog] = await Promise.all([
+                User.findById(userId, {}, { session }),
+                Blog.findById(blogId, {}, { session })
+            ])
+    
+            if (!user) return res.status(400).send({err: "user doest not exist"})
+            if (!blog) return res.status(400).send({err: "blog doest not exist"})
+    
+            if (!blog.islive) return res.status(400).send({err: "blog is not available"})
+            
+            comment = new Comment({
+                content,
+                user, 
+                userFullName:`${user.name.first} ${user.name.last}`, 
+                blog: blogId
+            });
+            // await session.abortTransaction();
+            // await Promise.all([
+            //     comment.save(),
+            //     Blog.updateOne({_id: blogId}, { $push: { comments: comment}})
+            // ]);
+            // await comment.save();
+            
+            blog.commentsCount++;
+            blog.comments.push(comment);
+            if (blog.commentsCount > 3) blog.comments.shift();
+            await Promise.all([
+                comment.save({ session }), 
+                blog.save()
+            ]);
         });
-        // await Promise.all([
-        //     comment.save(),
-        //     Blog.updateOne({_id: blogId}, { $push: { comments: comment}})
-        // ]);
-        // await comment.save();
-        
-        blog.commentsCount++;
-        blog.comments.push(comment);
-        if (blog.commentsCount > 3) blog.comments.shift();
-        await Promise.all([
-            comment.save(), 
-            blog.save()
-        ]);
-
-        return res.send({ comment })
+        return res.send({ comment });
     } catch(err) {
         console.log(err);
         return res.status(500).send({err: err.message})
+    } finally {
+        await session.endSession();
     }
 });
 
