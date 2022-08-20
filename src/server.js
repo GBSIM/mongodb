5dc20cf46b5a917f99ce1d3a1baaeb9dc0fa6395
gyubin <gbcompany27@gmail.com>
2022-08-20 15:31:04 +0900

    update port selection policy;

--- src/server.js
+++ src/server.js
@@ -7,8 +7,9 @@ const {generateFakeData} = require('../faker2');
 
 const server = async() => {
     try {
-        const { MONGO_URI } = process.env;
+        const { MONGO_URI, PORT } = process.env;
         if (!MONGO_URI) throw new Error("MONGO_URI is required!!");
+        if (!PORT) throw new Error("PORT is required!!");
 
         app.use(express.json());
         await mongoose.connect(MONGO_URI);
@@ -19,8 +20,8 @@ const server = async() => {
         app.use('/blog',blogRouter);
         app.use('/blog/:blogId/comment',commentRouter);
     
-        app.listen(3000,async () => {
-            console.log('server listening on port 3000');
+        app.listen(PORT,async () => {
+            console.log(`server listening on port ${PORT}`);
             // await generateFakeData(10, 2, 10);
         })
     } catch(err) {
